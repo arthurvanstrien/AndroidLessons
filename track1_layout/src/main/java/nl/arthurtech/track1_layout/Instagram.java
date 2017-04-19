3b0563a3e572f109a079cbac443269a472bc0433
ArthurvanStrien <arthurvanstrien@gmail.com>
2017-04-19 21:05:36 +0200

    created a module for a an assessment to create the layout of the login page of the instagram app.

---
+++ track1_layout/src/main/java/nl/arthurtech/track1_layout/Instagram.java
@@ -0,0 +1,13 @@
+package nl.arthurtech.track1_layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Instagram extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instagram);
+    }
+}
