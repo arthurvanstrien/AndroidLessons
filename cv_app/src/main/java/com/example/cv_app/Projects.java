5f60e0e60f4ead1de56e1136c45147b927d10e88
ArthurvanStrien <arthurvanstrien@gmail.com>
2017-07-03 16:59:54 +0200

    added the last projects.

--- cv_app/src/main/java/com/example/cv_app/Projects.java
+++ cv_app/src/main/java/com/example/cv_app/Projects.java
@@ -19,9 +19,9 @@ public class Projects extends AppCompatActivity
         ArrayList<ProjectListItem> projectListItemArrayList = new ArrayList<>();
         projectListItemArrayList.add(new ProjectListItem(R.string.projectNameWeatherStation, R.string.projectDescriptionWeatherStation, R.string.projectSDescriptionWeatherStation, R.drawable.ws_windspeed, R.drawable.ws_buitentemperatuur));
         projectListItemArrayList.add(new ProjectListItem(R.string.projectNameAGV, R.string.projectDescriptionAGV, R.string.projectSDescriptionAGV, R.drawable.boebot_3, R.drawable.boebot_1));
-        //projectListItemArrayList.add(new ProjectListItem(R.string.projectNameFestivalPlanner, R.string.projectDescriptionFestivalPlanner, R.string.projectSDescriptionFestivalPlanner, "something", "something"));
+        projectListItemArrayList.add(new ProjectListItem(R.string.projectNameFestivalPlanner, R.string.projectDescriptionFestivalPlanner, R.string.projectSDescriptionFestivalPlanner, R.drawable.festivalplanner2, R.drawable.festivalplanner3));
         projectListItemArrayList.add(new ProjectListItem(R.string.projectNameInteractiveApp, R.string.projectDescriptionInteractiveApp, R.string.projectSDescriptionInteractiveApp, R.drawable.coastercam, R.drawable.coastercam2));
-        //projectListItemArrayList.add(new ProjectListItem(R.string.projectNameCurriculumVitea, R.string.projectDescriptionCurriculumVitea, R.string.projectSDescriptionCurriculumVitea, "something", "something"));
+        projectListItemArrayList.add(new ProjectListItem(R.string.projectNameCurriculumVitea, R.string.projectDescriptionCurriculumVitea, R.string.projectSDescriptionCurriculumVitea, R.drawable.cvapp1, R.drawable.iconcv));
         projectListItemArrayList.add(new ProjectListItem(R.string.projectNameDoorbell, R.string.projectDescriptionDoorbell, R.string.projectSDescriptionDoorbell, R.drawable.p1420848, R.drawable.p1420846));
 
         super.onCreate(savedInstanceState);
